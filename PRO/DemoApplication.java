a6241b3d01b85a8f2c6bec6f9f5a4913d3ff6264
Anmol Upadhyay <73272750+upadhyaysaab@users.noreply.github.com>
2026-06-17 17:11:20 +0530

    Create DemoApplication.java

---
+++ PRO/DemoApplication.java
@@ -0,0 +1,17 @@
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.*;
+
+@SpringBootApplication
+@RestController
+public class DemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoApplication.class, args);
+    }
+
+    @GetMapping("/")
+    public String home() {
+        return "Hello World CI/CD Working 🚀";
+    }
+}
