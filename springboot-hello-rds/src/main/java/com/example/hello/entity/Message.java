a003c45878289e1c20fc3bc86b97a007e8df1d3c
Anmol Upadhyay <73272750+upadhyaysaab@users.noreply.github.com>
2026-06-26 16:28:43 +0530

    Create Message.java

---
+++ springboot-hello-rds/src/main/java/com/example/hello/entity/Message.java
@@ -0,0 +1,38 @@
+package com.example.hello.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name="messages")
+public class Message {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String text;
+
+    public Message() {
+    }
+
+    public Message(String text) {
+        this.text = text;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id=id;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text=text;
+    }
+
+}
