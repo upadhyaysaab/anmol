1d64db7e0829c4a8f1038d3937d45978e4d07e59
Anmol Upadhyay <73272750+upadhyaysaab@users.noreply.github.com>
2026-06-26 15:51:11 +0530

    Create App.js

---
+++ hello-react/src/App.js
@@ -0,0 +1,13 @@
+function App() {
+  return (
+    <div style={{ textAlign: "center", marginTop: "80px" }}>
+      <h1>Hello World 🚀</h1>
+
+      <h2>React App Running Successfully</h2>
+
+      <p>Served via Nginx on AWS EC2</p>
+    </div>
+  );
+}
+
+export default App;
